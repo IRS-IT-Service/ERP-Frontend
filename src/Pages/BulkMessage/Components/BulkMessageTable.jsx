131cedc75a5b1b51a8108348a798075f6560b4ff
IRS-Github <irspvtltd7@gmail.com>
2024-02-17 17:08:58 +0530

    Riseev done bulk message

--- src/Pages/BulkMessage/Components/BulkMessageTable.jsx
+++ src/Pages/BulkMessage/Components/BulkMessageTable.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   styled,
   Button,
@@ -14,6 +14,8 @@ import {
 } from "@mui/material";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import { DataGrid } from "@mui/x-data-grid";
+import { toast } from "react-toastify";
+import { Numbers } from "@mui/icons-material";
 //File upload
 const VisuallyHiddenInput = styled("input")({
   clip: "rect(0 0 0 0)",
@@ -30,6 +32,7 @@ const VisuallyHiddenInput = styled("input")({
 const BulkMessageTable = () => {
   const [open, setOpen] = useState(false);
   const [fileUploaded, setFileUploaded] = useState(false);
+  const [customerNumber, setCustomerNumber] = useState([]);
 
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
@@ -45,13 +48,22 @@ const BulkMessageTable = () => {
   const handleClose = () => {
     setOpen(false);
   };
-
   const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("Customer Name:", input.CustomerName);
-    console.log("Customer Number:", input.CustomerNumber);
-    setInput([]);
-    handleClose();
+    const customerNumberRegex = /^\d{10}$/;
+
+    if (!input.CustomerName || !input.CustomerNumber) {
+      toast.error("Please fill Customer Name or Customer Number ");
+      event.preventDefault();
+    } else if (!customerNumberRegex.test(input.CustomerNumber)) {
+      toast.error("Please Enter Correct Customer Number");
+      event.preventDefault();
+    } else {
+      event.preventDefault();
+      console.log("Customer Name:", input.CustomerName);
+      console.log("Customer Number:", input.CustomerNumber);
+      setInput([]);
+      handleClose();
+    }
   };
 
   const handleInputChange = (event) => {
@@ -61,6 +73,15 @@ const BulkMessageTable = () => {
       [name]: value,
     }));
   };
+
+  const handleSelectionChange = (selectionModel) => {
+    const selectedCustomerNumbers = selectionModel.map((selectedRowId) => {
+      const selectedRow = data.find((row) => row.id === selectedRowId);
+      return selectedRow ? selectedRow.CustomerNumber : null;
+    });
+    setCustomerNumber(selectedCustomerNumbers);
+  };
+  console.log(" Selected Customer Numbers:", customerNumber);
 
   //This is data grid data
   const columns = [
@@ -78,8 +99,7 @@ const BulkMessageTable = () => {
       editable: true,
     },
   ];
-
-  const rows = [
+  const [data, setDate] = useState([
     { id: 1, CustomerName: "John", CustomerNumber: "12345" },
     { id: 2, CustomerName: "Alice", CustomerNumber: "67890" },
     { id: 3, CustomerName: "Bob", CustomerNumber: "24680" },
@@ -100,157 +120,154 @@ const BulkMessageTable = () => {
     { id: 18, CustomerName: "Harper", CustomerNumber: "50968" },
     { id: 19, CustomerName: "Ethan", CustomerNumber: "81739" },
     { id: 20, CustomerName: "Evelyn", CustomerNumber: "26374" },
-  ];
+  ]);
 
   return (
-    <div>
-      <Box sx={{ display: "flex" }}>
+    <Box sx={{ display: "flex" }}>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          justifyContent: "space-around",
+        }}
+      >
         <Box
           sx={{
+            width: "30%",
             display: "flex",
-            width: "100%",
-            justifyContent: "space-around",
+            flexDirection: "column",
+            justifyItems: "center",
+            overflow: "hidden",
           }}
         >
+          <Button
+            variant="outlined"
+            onClick={handleClickOpen}
+            sx={{ margin: "6px 0px 6px 0px " }}
+          >
+            Add Customer
+          </Button>
+          <TextareaAutosize
+            minRows={8}
+            placeholder="Enter your message"
+            aria-label="maximum height"
+          />
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            PaperProps={{
+              component: "form",
+              onSubmit: handleSubmit,
+            }}
+          >
+            <DialogTitle
+              id="alert-dialog-title"
+              sx={{
+                textAlign: "center",
+                display: "flex",
+                justifyContent: "space-around",
+              }}
+            >
+              <Typography sx={{ fontWeight: "bold" }}>Add Customer</Typography>
+            </DialogTitle>
+
+            <DialogContent>
+              <Box
+                sx={{
+                  gap: "12px",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "10px",
+                  }}
+                >
+                  <TextField
+                    id="outlined-basic"
+                    label="Customer Name"
+                    variant="outlined"
+                    sx={{ width: "100%", marginTop: "12px" }}
+                    name="CustomerName"
+                    value={input.CustomerName}
+                    onChange={handleInputChange}
+                  />
+                  <TextField
+                    id="outlined-basic"
+                    label="Customer Number"
+                    variant="outlined"
+                    sx={{ width: "100%" }}
+                    name="CustomerNumber"
+                    value={input.CustomerNumber}
+                    onChange={handleInputChange}
+                    type="number"
+                  />
+
+                  <Button type="submit" variant="outlined">
+                    Add
+                  </Button>
+                  <Button
+                    onClick={handleClose}
+                    variant="outlined"
+                    sx={{ fontWeight: "bold" }}
+                  >
+                    {" "}
+                    Cancel
+                  </Button>
+                </Box>
+              </Box>
+            </DialogContent>
+            <DialogActions></DialogActions>
+          </Dialog>
           <Box
             sx={{
-              width: "30%",
               display: "flex",
-              flexDirection: "column",
-              justifyItems: "center",
-              overflow: "hidden",
+              justifyContent: "space-around",
+              alignItems: "center",
+              margin: "10px",
             }}
           >
             <Button
-              variant="outlined"
-              onClick={handleClickOpen}
-              sx={{ margin: "6px 0px 6px 0px " }}
+              component="label"
+              role={undefined}
+              variant="contained"
+              tabIndex={-1}
+              startIcon={<CloudUploadIcon />}
+              style={{ backgroundColor: fileUploaded ? "green" : undefined }}
             >
-              Add Customer
+              {fileUploaded ? "File Uploaded" : "Upload File"}
+              <VisuallyHiddenInput type="file" onChange={handleFileUpload} />
             </Button>
-            <TextareaAutosize
-              minRows={8}
-              placeholder="Enter your message"
-              aria-label="maximum height"
-            />
-            <Dialog
-              open={open}
-              onClose={handleClose}
-              PaperProps={{
-                component: "form",
-                onSubmit: handleSubmit,
-              }}
-            >
-              <DialogTitle
-                id="alert-dialog-title"
-                sx={{
-                  textAlign: "center",
-                  display: "flex",
-                  justifyContent: "space-around",
-                }}
-              >
-                <Typography sx={{ fontWeight: "bold" }}>
-                  Add Customer
-                </Typography>
-              </DialogTitle>
-
-              <DialogContent>
-                <Box
-                  sx={{
-                    gap: "12px",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <Box
-                    sx={{
-                      display: "flex",
-                      flexDirection: "column",
-                      gap: "10px",
-                    }}
-                  >
-                    <TextField
-                      id="outlined-basic"
-                      label="Customer Name"
-                      variant="outlined"
-                      sx={{ width: "100%", marginTop: "12px" }}
-                      name="CustomerName"
-                      value={input.CustomerName}
-                      onChange={handleInputChange}
-                    />
-                    <TextField
-                      id="outlined-basic"
-                      label="Customer Number"
-                      variant="outlined"
-                      text="number"
-                      sx={{ width: "100%" }}
-                      name="CustomerNumber"
-                      value={input.CustomerNumber}
-                      onChange={handleInputChange}
-                    />
-
-                    <Button type="submit" variant="outlined">
-                      Add
-                    </Button>
-                    <Button
-                      onClick={handleClose}
-                      variant="outlined"
-                      sx={{ fontWeight: "bold" }}
-                    >
-                      {" "}
-                      Cancel
-                    </Button>
-                  </Box>
-                </Box>
-              </DialogContent>
-              <DialogActions></DialogActions>
-            </Dialog>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-around",
-                alignItems: "center",
-                margin: "10px",
-              }}
-            >
-              <Button
-                component="label"
-                role={undefined}
-                variant="contained"
-                tabIndex={-1}
-                startIcon={<CloudUploadIcon />}
-                style={{ backgroundColor: fileUploaded ? "green" : undefined }}
-              >
-                {fileUploaded ? "File Uploaded" : "Upload File"}
-                <VisuallyHiddenInput type="file" onChange={handleFileUpload} />
-              </Button>
-              <Button variant="outlined" sx={{ margin: "10px" }}>
-                Send
-              </Button>
-            </Box>
+            <Button variant="outlined" sx={{ margin: "10px" }}>
+              Send
+            </Button>
           </Box>
-          <Box sx={{ width: "35%" }}>
-            <Box sx={{ height: "72vh", width: "100%" }}>
-              <DataGrid
-                rows={rows}
-                columns={columns}
-                rowHeight={50}
-                initialState={{
-                  pagination: {
-                    paginationModel: {
-                      pageSize: 10,
-                    },
+        </Box>
+        <Box sx={{ width: "35%" }}>
+          <Box sx={{ height: "72vh", width: "100%" }}>
+            <DataGrid
+              rows={data}
+              columns={columns}
+              rowHeight={50}
+              initialState={{
+                pagination: {
+                  paginationModel: {
+                    pageSize: 10,
                   },
-                }}
-                pageSizeOptions={[10]}
-                checkboxSelection
-                disableRowSelectionOnClick
-              />
-            </Box>
+                },
+              }}
+              pageSizeOptions={[10]}
+              checkboxSelection
+              disableRowSelectionOnClick
+              onRowSelectionModelChange={handleSelectionChange}
+            />
           </Box>
         </Box>
       </Box>
-    </div>
+    </Box>
   );
 };
 
