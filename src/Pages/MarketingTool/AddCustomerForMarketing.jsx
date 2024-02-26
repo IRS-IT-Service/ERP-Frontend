8343a7f54c32f60238cf305a7e3b8c5e76bb09ca
IRS-IT-Service <irsitservice@gmail.com>
2024-02-26 17:15:03 +0530

    sends and fetches data through api

--- src/Pages/MarketingTool/AddCustomerForMarketing.jsx
+++ src/Pages/MarketingTool/AddCustomerForMarketing.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import {
   Box,
   styled,
@@ -6,77 +6,87 @@ import {
   CircularProgress,
   Grid,
   TextField,
-} from "@mui/material";
-import Header from "../../components/Common/Header";
-import axios from "axios";
-import { DataGrid } from "@mui/x-data-grid";
-import BASEURL from "../../constants/BaseApi";
-import * as XLSX from "xlsx";
-import { toast } from "react-toastify";
-
-const DrawerHeader = styled("div")(({ theme }) => ({
+} from '@mui/material';
+import Header from '../../components/Common/Header';
+import axios from 'axios';
+import { DataGrid } from '@mui/x-data-grid';
+import BASEURL from '../../constants/BaseApi';
+import * as XLSX from 'xlsx';
+import { toast } from 'react-toastify';
+import { useAddCustomerNumberMutation } from '../../features/api/whatsAppApiSlice';
+const DrawerHeader = styled('div')(({ theme }) => ({
   ...theme.mixins.toolbar,
 }));
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+// import { IoMdArrowBack } from 'react-icons/io';
+
 
 const columns = [
   {
-    field: "Sno",
-    headerClassName: "super-app-theme--header",
-    cellClassName: "super-app-theme--cell",
+    field: 'Sno',
+    headerClassName: 'super-app-theme--header',
+    cellClassName: 'super-app-theme--cell',
     flex: 0.3,
-    headerName: "Sno",
+    headerName: 'Sno',
     width: 100,
   },
   {
-    field: "CustomerName",
-    headerClassName: "super-app-theme--header",
-    cellClassName: "super-app-theme--cell",
+    field: 'CustomerName',
+    headerClassName: 'super-app-theme--header',
+    cellClassName: 'super-app-theme--cell',
     flex: 0.3,
-    headerName: "Customer Name",
+    headerName: 'Customer Name',
     width: 350,
   },
   {
-    field: "CompanyName",
-    headerClassName: "super-app-theme--header",
-    cellClassName: "super-app-theme--cell",
+    field: 'CompanyName',
+    headerClassName: 'super-app-theme--header',
+    cellClassName: 'super-app-theme--cell',
     flex: 0.3,
-    headerName: "Company Name",
+    headerName: 'Company Name',
     width: 250,
   },
 
   {
-    field: "MobileNo",
-    headerClassName: "super-app-theme--header",
-    cellClassName: "super-app-theme--cell",
+    field: 'CustomerNumber',
+    headerClassName: 'super-app-theme--header',
+    cellClassName: 'super-app-theme--cell',
     flex: 0.3,
-    headerName: "Mobile No",
+    headerName: 'Mobile No',
     width: 250,
   },
   {
-    field: "Address",
-    headerClassName: "super-app-theme--header",
-    cellClassName: "super-app-theme--cell",
+    field: 'Address',
+    headerClassName: 'super-app-theme--header',
+    cellClassName: 'super-app-theme--cell',
     flex: 0.3,
-    headerName: "Address",
+    headerName: 'Address',
     width: 250,
   },
 ];
 
 const AddCustomerForMarketing = () => {
+  //rtk for adding customer
+  const [addCustomerNumberData] = useAddCustomerNumberMutation();
+
   const [data, setData] = useState([
     {
       Sno: 1,
       id: 1,
-      CustomerName: "",
-      MobileNo: "",
-      CompanyName: "",
-      Address: "",
+      CustomerName: '',
+      CustomerNumber: '',
+      CompanyName: '',
+      Address: '',
     },
   ]);
   const [loading, setLoading] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [excelData, setExcelData] = useState([]);
   const [submitData, setSubmitData] = useState([]);
+  const navigate = useNavigate();
+  const { themeColor } = useSelector((state) => state.ui);
+   const color = themeColor.sideBarColor1;
   // const handle change for data input
 
   const handleChange = (e, index) => {
@@ -106,33 +116,33 @@ const AddCustomerForMarketing = () => {
     });
 
     setSubmitData([...updatedSubmitData, ...updatedData]);
+
+    // to clear the input
     setData([
       {
         Sno: updatedSubmitData.length + 1,
-        CustomerName: "",
-        MobileNo: "",
-        CompanyName: "",
-        Address: "",
+        CustomerName: '',
+        CustomerNumber: '',
+        CompanyName: '',
+        Address: '',
       },
     ]);
   };
   const handleFileChange = (event) => {
-
-   
     const file = event.target.files[0];
     const reader = new FileReader();
 
     reader.onload = (e) => {
       const data = new Uint8Array(e.target.result);
-      const workbook = XLSX.read(data, { type: "array" });
+      const workbook = XLSX.read(data, { type: 'array' });
       const sheetName = workbook.SheetNames[0];
       const worksheet = workbook.Sheets[sheetName];
       const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
       // Remove white spaces from the header row
       const headerRow = jsonData.shift().map((item) => item.trim());
       const processedHeaderRow = headerRow.map((item) =>
-        item.startsWith("Name")
-          ? item.replace(" (its not required for reference only)", "").trim()
+        item.startsWith('Name')
+          ? item.replace(' (its not required for reference only)', '').trim()
           : item
       );
 
@@ -141,7 +151,7 @@ const AddCustomerForMarketing = () => {
           (obj, value, columnIndex) => {
             // Remove white spaces from the cell values
             const trimmedValue =
-              typeof value === "string" ? value.trim() : value;
+              typeof value === 'string' ? value.trim() : value;
             return {
               ...obj,
               [processedHeaderRow[columnIndex]]: trimmedValue,
@@ -164,33 +174,44 @@ const AddCustomerForMarketing = () => {
       const response = await axios.get(
         `${BASEURL}/Sample/CustomersSample.xlsx`,
         {
-          responseType: "blob",
+          responseType: 'blob',
         }
       );
 
       // Create a temporary link element to trigger the download
       const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement("a");
+      const link = document.createElement('a');
       link.href = url;
-      link.setAttribute("download", "CustomersSample.xlsx");
+      link.setAttribute('download', 'CustomersSample.xlsx');
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
     } catch (error) {
-      console.error("Error downloading sample:", error);
+      console.error('Error downloading sample:', error);
     } finally {
       setLoading(false);
     }
   };
-  
-  const handleSubmit = () => {
-    if (submitData.length === 0 || excelData.length === 0) {
-      return toast.error(
-        "Please add some values or upload excel file before submitting"
-      );
-    }
 
+  const handleSubmit = async () => {
+    if (submitData.length === 0 && excelData.length === 0) {
+     return toast.error('add customer detail first')
+    }
+     const info = { data: submitData.length > 0 ? submitData : excelData };
+     console.log('hi from info');
+
+     try {
+       const res = await addCustomerNumberData(info).unwrap();
+       toast.success('Customer added Successfully');
+     } catch (error) {
+       toast.error(error);
+     }
   };
+
+  const handleNavigation = () => {
+    navigate('/BulkMessage');
+  };
+  
   return (
     <>
       <Box
@@ -203,11 +224,20 @@ const AddCustomerForMarketing = () => {
           sx={{
             width: '100%',
             display: 'flex',
-            justifyContent: 'space-around',
+            justifyContent: 'space-between',
             alignItems: 'center',
             marginY: '5px',
           }}
         >
+          <Button
+            variant='contained'
+            size='small'
+            sx={{ background: color }}
+            onClick={handleNavigation}
+          >
+            {/* <IoMdArrowBack /> */}
+            back to Bulk Message
+          </Button>
           <Box>
             <input
               type='file'
@@ -215,17 +245,16 @@ const AddCustomerForMarketing = () => {
               onChange={handleFileChange}
               style={{ display: 'none' }}
               id='file-upload'
-              // disabled={submitData.length > 0 ? true : false}
+              disabled={submitData.length > 0 ? true : false}
             />
             <label htmlFor='file-upload'>
               <Button
                 sx={{
-                  '&:hover': {
-                    backgroundColor: 'black',
-                  },
+                  backgroundColor: color,
                 }}
                 variant='contained'
                 component='span'
+                disabled={submitData.length > 0 ? true : false}
               >
                 Upload Excel File
               </Button>
@@ -234,9 +263,7 @@ const AddCustomerForMarketing = () => {
           <Button
             variant='contained'
             sx={{
-              '&:hover': {
-                backgroundColor: 'black',
-              },
+              backgroundColor: color,
             }}
             onClick={handleSubmit}
           >
@@ -253,10 +280,7 @@ const AddCustomerForMarketing = () => {
           <Button
             variant='contained'
             sx={{
-              // backgroundColor: themeColor.sideBarColor1,
-              '&:hover': {
-                backgroundColor: 'black',
-              },
+              backgroundColor: color,
             }}
             onClick={donwnloadExcelSample}
           >
@@ -314,8 +338,8 @@ const AddCustomerForMarketing = () => {
                 <TextField
                   label='Mobile Number'
                   fullWidth
-                  name='MobileNo'
-                  value={item.MobileNo}
+                  name='CustomerNumber'
+                  value={item.CustomerNumber}
                   onChange={(e) => handleChange(e, index)}
                 />
               </Grid>
@@ -331,8 +355,9 @@ const AddCustomerForMarketing = () => {
 
               <Grid item sm={1}>
                 <Button
-                  // disabled={(excelData.length > 0) ? true : false}
+                  disabled={excelData.length > 0 ? true : false}
                   variant='contained'
+                  sx={{background: color}}
                   onClick={() => handleAddData()}
                 >
                   Add
