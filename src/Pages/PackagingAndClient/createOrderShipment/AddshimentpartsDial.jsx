24bcc53e87b2b660fff3dbd4a68f9c60377d5745
IRS-IT-Service <irsitservice@gmail.com>
2024-05-21 14:57:22 +0530

    hello

--- src/Pages/PackagingAndClient/createOrderShipment/AddshimentpartsDial.jsx
+++ src/Pages/PackagingAndClient/createOrderShipment/AddshimentpartsDial.jsx
@@ -18,6 +18,11 @@ import {
   styled,
   InputAdornment,
   Autocomplete,
+  Popover,
+  ListItemButton,
+  ListItemIcon,
+  Checkbox,
+  ListItemText
 } from "@mui/material";
 import CancelIcon from "@mui/icons-material/Cancel";
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -77,12 +82,51 @@ const AddshipmentDial = ({
   refetch,
 }) => {
   /// initialize
+
+
+  const addresses = [
+    {
+      SNO:1,
+      city: "ali village",
+      district: "south delhi",
+      state: "New Delhi",
+      country: "India",
+      addressLine1: "Sarita vihar",
+      addressLine2: "Delhi",
+      pincode: "814133"
+    },
+    {
+      SNO:2,
+      city: "another city",
+      district: "another district",
+      state: "another state",
+      country: "another country",
+      addressLine1: "another address line ",
+      addressLine2: "another address line 2",
+      pincode: "another pincode"
+    }
+    // Add more address objects as needed
+  ];
+
+const permanent ={
+  
+    SNO:1,
+    city: "ali village",
+    district: "south delhi",
+    state: "New Delhi",
+    country: "India",
+    addressLine1: "Sarita vihar",
+    addressLine2: "Delhi",
+    pincode: "814133"
+  
+}
   const socket = useSocket();
   const navigate = useNavigate();
 
   /// global state
   const { userInfo } = useSelector((state) => state.auth);
   const { Addparts } = useSelector((state) => state.RAndDForm);
+  const [anchorEl, setAnchorEl] = useState(null);
 
   /// local state
 
@@ -91,6 +135,9 @@ const AddshipmentDial = ({
   const [qty, setQty] = useState({});
   const [finalData, setFinalData] = useState([{}]);
   const [FinalPreorder, setFinalPreorder] = useState({});
+  const [selectedAddress, setSelectedAddress] = useState({});
+  const [addAddress ,setAddaddress] = useState(false);
+
 
   /// rtk query
 
@@ -116,7 +163,21 @@ const AddshipmentDial = ({
     setRequireqty(newData);
   }, [setOpen, open]);
 
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+const handleAddaddress = (event) => {
+  setAddaddress(!addAddress);
+}
+
+
+  const openPop = Boolean(anchorEl);
+  const idPop = openPop ? 'simple-popover' : undefined;
   //Reset value after delete elements
   useEffect(() => {
     const matchingArray = [];
@@ -161,7 +222,13 @@ const AddshipmentDial = ({
   };
 
 
-  console.log(Requireqty)
+
+
+  const handleSelectAddress = (address) => {
+    setSelectedAddress(address);
+    handleClose();
+  };
+
   useEffect(() => {
     return () => {
       removeSelectedItems([]);
@@ -342,28 +409,38 @@ const AddshipmentDial = ({
                   gap: "10px",
                 }}
               >
+                <Box>
                 <Typography variant="span" fontWeight="bold" fontSize={"12px"}>
-                  Billing Address{" "}
+                  Billing Address:{" "}
                 </Typography>
-                <Autocomplete
-                style={{
-                  width: "50%",
-                  backgroundColor: "rgba(255, 255, 255)",
-                }}
-                options={data?.data || []}
-                getOptionLabel={(option) => option.ModelName}
-                // onChange={handleSelectedChange}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    label="Select"
-                    onChange={(e) => {
-                      console.log(e.target.value);
-                    }}
-                    size="small"
-                  />
-                )}
-              />
+
+                <Box sx={{
+
+      width: "100%",
+      height: "auto",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      padding: "16px",
+      marginTop: "16px"
+    }}>
+      <Table>
+        <TableBody>
+          {Object.keys(permanent).map((key) => {
+            if (key === "SNO") {
+              return null;
+            }
+            return (
+              <TableRow key={key} sx={{ padding: 0  }}>
+                <TableCell sx={{ padding: 0.5, fontWeight: "bold" }}>{key?.toUpperCase()}:</TableCell>
+                <TableCell sx={{ padding: 0  }}>{permanent[key]}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Box>
+    </Box>
               </Grid>
               <Grid
                 item
@@ -375,32 +452,67 @@ const AddshipmentDial = ({
                   gap: "10px",
                 }}
               >
+                <Box sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  gap: "10px",
+                }}>
+                  <Box sx={{
+                    display:"flex",
+                    gap:"10px",
+                    alignItems: "center",
+                    borderRadius:"25px",
+                    padding:"5px",
+                  }}>
                 <Typography variant="span" fontWeight="bold" fontSize={"12px"}>
                   Shipping Address{" "}
                 </Typography>
-                <Autocomplete
-                style={{
-                  width: "50%",
-                  backgroundColor: "rgba(255, 255, 255)",
-                }}
-                options={data?.data || []}
-                getOptionLabel={(option) => option.ModelName}
-                // onChange={handleSelectedChange}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    label="Select"
-                    onChange={(e) => {
-                      console.log(e.target.value);
-                    }}
-                    size="small"
-                  />
-                )}
-              />
+     <Box sx={{
+         cursor: "pointer",
+         color: "blue",
+         fontSize: "20px",
+         "&:hover": {
+           color: "red",
+         },
+     }} onClick={handleClick}> <i className="fa-solid fa-plus" ></i> </Box>
+       
+      </Box>
+      <Box sx={{
+ 
+        width: "100%",
+        height: "auto",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        padding: "16px",
+        }}>
+        {selectedAddress ? (
+          <Table>
+            <TableBody>
+              {Object.keys(selectedAddress).map((key) => {
+                if (key === "SNO") {
+                  return null;
+                }
+                return (
+                  <TableRow key={key} sx={{ padding: 0 }}>
+                    <TableCell sx={{ padding: 0.5 ,fontWeight:"bold" }}>{key?.toLocaleUpperCase()}:</TableCell>
+                    <TableCell sx={{ padding: 0 }}>{selectedAddress[key]}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        ) : (
+          <Typography>No address selected</Typography>
+        )}
+      </Box>
+      </Box>
               </Grid>
             </Grid>
           </Box>
-          <TableContainer sx={{ maxHeight: "60vh", marginTop: "0.3rem" }}>
+          <TableContainer sx={{ maxHeight: "41vh", marginTop: "0.3rem" }}>
             <Table stickyHeader aria-label="sticky table">
               <TableHead>
                 <TableRow>
@@ -519,6 +631,96 @@ const AddshipmentDial = ({
             </Button>
           </Box>
         </StyledBox>
+      {/* Address popover */}
+        <Popover
+        id={idPop}
+        open={openPop}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+      >
+
+        {addAddress ? 
+        <Box sx={{
+          width:"100%",
+          height:"auto",
+          padding:"10px"
+        }}>
+              <Table>
+          <TableBody>
+    
+        <TableRow  sx={{ padding: 0 }}>
+                    <TableCell sx={{ padding: 0.5 ,fontWeight:"bold" }}>PINCODE</TableCell>
+                    <TableCell sx={{ padding: 1 }}><TextField size="small"/></TableCell>
+                  </TableRow>
+                  <TableRow  sx={{ padding: 0 }}>
+                    <TableCell sx={{ padding: 0.5 ,fontWeight:"bold" }}>DISTRICT</TableCell>
+                    <TableCell sx={{ padding: 1 }}><TextField size="small"/></TableCell>
+                  </TableRow>
+                  <TableRow  sx={{ padding: 0 }}>
+                    <TableCell sx={{ padding: 0.5 ,fontWeight:"bold" }}>STATE</TableCell>
+                    <TableCell sx={{ padding: 1 }}><TextField size="small"/></TableCell>
+                  </TableRow>
+                  <TableRow  sx={{ padding: 0 }}>
+                    <TableCell sx={{ padding: 0.5 ,fontWeight:"bold" }}>COUNTRY</TableCell>
+                    <TableCell sx={{ padding: 1 }}><TextField size="small"/></TableCell>
+                  </TableRow>
+                  <TableRow  sx={{ padding: 0 }}>
+                    <TableCell sx={{ padding: 0.5 ,fontWeight:"bold" }}>ADDRESS-LINE-1</TableCell>
+                    <TableCell sx={{ padding: 1 }}><TextField size="small"/></TableCell>
+                  </TableRow>
+                  <TableRow  sx={{ padding: 0 }}>
+                    <TableCell sx={{ padding: 0.5 ,fontWeight:"bold" }}>ADDRESS-LINE-1</TableCell>
+                    <TableCell sx={{ padding: 1 }}><TextField size="small"/></TableCell>
+                  </TableRow>
+                  </TableBody>
+                  </Table>
+                  </Box>
+          : addresses.map((item, index) => (
+          <ListItemButton
+            key={index}
+            sx={{
+              p: 2,
+              background: selectedAddress.SNO === item.SNO ? 'lightblue' : 'white',
+          
+              
+            }}
+            onClick={() => handleSelectAddress(item)}
+          >
+            <ListItemIcon>
+              <Checkbox
+                edge="start"
+                checked={selectedAddress.SNO === item.SNO}
+                tabIndex={-1}
+                disableRipple
+              />
+            </ListItemIcon>
+            <ListItemText sx={{
+            
+              width:"400px",
+              display:"flex",
+              flexWrap:"wrap"
+            }}
+              primary={`${item.addressLine1}, ${item.addressLine2}, ${item.city}, ${item.state}, ${item.country}, ${item.pincode}`}
+            />
+          </ListItemButton>
+        ))}
+        <Box sx={{
+    display: 'flex',
+    justifyContent:"end",
+ width:"100%",
+ padding:"5px"
+
+
+
+  }}>
+       <Button onClick={handleAddaddress} >{addAddress ? "Save" : "Add more Address"}</Button>
+       </Box>
+      </Popover>
+     
       </Dialog>
     </div>
   );
