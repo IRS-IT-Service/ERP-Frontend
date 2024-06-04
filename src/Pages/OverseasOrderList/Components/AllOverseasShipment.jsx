7c2d1ebf09eb6f0897b5d7806f2b1846dd1f270a
IRS-IT-Service <irsitservice@gmail.com>
2024-06-04 10:43:55 +0530

    final

--- src/Pages/OverseasOrderList/Components/AllOverseasShipment.jsx
+++ src/Pages/OverseasOrderList/Components/AllOverseasShipment.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef } from 'react';
 
 import {
   Button,
@@ -10,33 +10,32 @@ import {
   Table,
   TableRow,
   TableCell,
-} from "@mui/material";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
-import { DataGrid } from "@mui/x-data-grid";
-import Nodata from "../../../assets/error.gif";
-import Loading from "../../../components/Common/Loading";
-import { makeStyles } from "@mui/styles";
-import { formatDate } from "../../../commonFunctions/commonFunctions";
-import { useGetAllOverseasShipmentQuery } from "../../../features/api/RestockOrderApiSlice";
+} from '@mui/material';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { DataGrid } from '@mui/x-data-grid';
+import Nodata from '../../../assets/error.gif';
+import Loading from '../../../components/Common/Loading';
+import { makeStyles } from '@mui/styles';
+import { formatDate } from '../../../commonFunctions/commonFunctions';
+import { useGetAllOverseasShipmentQuery } from '../../../features/api/RestockOrderApiSlice';
 
-import Header from "../../../components/Common/Header";
-import { useDispatch, useSelector } from "react-redux";
-import { setHeader, setInfo } from "../../../features/slice/uiSlice";
-import InfoDialogBox from "../../../components/Common/InfoDialogBox";
+import Header from '../../../components/Common/Header';
+import { useDispatch, useSelector } from 'react-redux';
+import { setHeader, setInfo } from '../../../features/slice/uiSlice';
+import InfoDialogBox from '../../../components/Common/InfoDialogBox';
 
-const DrawerHeader = styled("div")(({ theme }) => ({
+const DrawerHeader = styled('div')(({ theme }) => ({
   ...theme.mixins.toolbar,
 }));
 const StyledBox = styled(Box)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
 }));
 const useStyles = makeStyles((theme) => ({
   selected: {
-    backgroundColor: "rgb(4,4,61) !important",
-    color: "white !important",
+    backgroundColor: 'rgb(4,4,61) !important',
+    color: 'white !important',
   },
 }));
-
 
 //Todo
 const infoDetail = [
@@ -55,13 +54,12 @@ const infoDetail = [
     name: 'Details',
     screenshot: (
       <img
-        src='https://ik.imagekit.io/z7h0zeety/Admin-Portal/Info%20SS%20images/view_wholesale%20request.png?updatedAt=1703065319338'
+        src='https://ik.imagekit.io/z7h0zeety/Admin-Portal/Info%20SS%20images/btnRecieved.png?updatedAt=1717405073807'
         height={'100%'}
         width={'100%'}
       />
     ),
-    instruction:
-      'If you click to View Button Under Details so you can see One overseas Shipment with all the Shipment Details',
+    instruction: '',
   },
 ];
 
@@ -73,12 +71,12 @@ const AllOverseasShipment = () => {
   const id = useParams().id;
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const companyName = queryParams.get("companyName");
-  const concernPerson = queryParams.get("concernPerson");
-  const mobile = queryParams.get("mobile");
+  const companyName = queryParams.get('companyName');
+  const concernPerson = queryParams.get('concernPerson');
+  const mobile = queryParams.get('mobile');
 
   /// local state
-  const [toggleValue, setToggleValue] = useState("pending");
+  const [toggleValue, setToggleValue] = useState('pending');
   const [rows, setRows] = useState([]);
 
   /// RTK query
@@ -109,97 +107,97 @@ const AllOverseasShipment = () => {
   // Define the columns
   const columns = [
     {
-      field: "Sno",
-      headerName: "Sno",
+      field: 'Sno',
+      headerName: 'Sno',
       flex: 0.1,
       minWidth: 10,
-      align: "center",
-      headerAlign: "center",
-      headerClassName: "super-app-theme--header",
-      cellClassName: "super-app-theme--cell",
+      align: 'center',
+      headerAlign: 'center',
+      headerClassName: 'super-app-theme--header',
+      cellClassName: 'super-app-theme--cell',
     },
     {
-      field: "trackingId",
-      headerName: "TrackingId",
+      field: 'trackingId',
+      headerName: 'TrackingId',
       flex: 0.3,
       minWidth: 100,
-      align: "center",
-      headerAlign: "center",
-      headerClassName: "super-app-theme--header",
-      cellClassName: "super-app-theme--cell",
+      align: 'center',
+      headerAlign: 'center',
+      headerClassName: 'super-app-theme--header',
+      cellClassName: 'super-app-theme--cell',
     },
     {
-      field: "courierName",
-      headerName: "Courier Name",
+      field: 'courierName',
+      headerName: 'Courier Name',
       flex: 0.3,
       minWidth: 100,
-      align: "center",
-      headerAlign: "center",
-      headerClassName: "super-app-theme--header",
-      cellClassName: "super-app-theme--cell",
+      align: 'center',
+      headerAlign: 'center',
+      headerClassName: 'super-app-theme--header',
+      cellClassName: 'super-app-theme--cell',
     },
     {
-      field: "shipmentDate",
-      headerName: "Date",
+      field: 'shipmentDate',
+      headerName: 'Date',
       flex: 0.3,
       minWidth: 50,
-      align: "center",
-      headerAlign: "center",
-      headerClassName: "super-app-theme--header",
-      cellClassName: "super-app-theme--cell",
+      align: 'center',
+      headerAlign: 'center',
+      headerClassName: 'super-app-theme--header',
+      cellClassName: 'super-app-theme--cell',
     },
     {
-      field: "forwarder",
-      headerName: "Forwarder",
+      field: 'forwarder',
+      headerName: 'Forwarder',
       flex: 0.2,
       minWidth: 50,
-      align: "center",
-      headerAlign: "center",
-      headerClassName: "super-app-theme--header",
-      cellClassName: "super-app-theme--cell",
+      align: 'center',
+      headerAlign: 'center',
+      headerClassName: 'super-app-theme--header',
+      cellClassName: 'super-app-theme--cell',
     },
 
     {
-      field: "noOfBoxes",
-      headerName: "No Boxes",
+      field: 'noOfBoxes',
+      headerName: 'No Boxes',
       flex: 0.2,
       minWidth: 50,
-      align: "center",
-      headerAlign: "center",
-      headerClassName: "super-app-theme--header",
-      cellClassName: "super-app-theme--cell",
+      align: 'center',
+      headerAlign: 'center',
+      headerClassName: 'super-app-theme--header',
+      cellClassName: 'super-app-theme--cell',
     },
 
     {
-      field: "comment",
-      headerName: "Comment",
+      field: 'comment',
+      headerName: 'Comment',
       flex: 0.2,
       minWidth: 50,
-      align: "center",
-      headerAlign: "center",
-      headerClassName: "super-app-theme--header",
-      cellClassName: "super-app-theme--cell",
+      align: 'center',
+      headerAlign: 'center',
+      headerClassName: 'super-app-theme--header',
+      cellClassName: 'super-app-theme--cell',
     },
     {
-      field: "status",
-      headerName: "Status",
+      field: 'status',
+      headerName: 'Status',
       flex: 0.2,
       minWidth: 50,
-      align: "center",
-      headerAlign: "center",
-      headerClassName: "super-app-theme--header",
-      cellClassName: "super-app-theme--cell",
+      align: 'center',
+      headerAlign: 'center',
+      headerClassName: 'super-app-theme--header',
+      cellClassName: 'super-app-theme--cell',
     },
 
     {
-      field: "details",
-      headerName: "Details",
+      field: 'details',
+      headerName: 'Details',
       sortable: false,
       minWidth: 100,
-      align: "center",
-      headerAlign: "center",
-      headerClassName: "super-app-theme--header",
-      cellClassName: "super-app-theme--cell",
+      align: 'center',
+      headerAlign: 'center',
+      headerClassName: 'super-app-theme--header',
+      cellClassName: 'super-app-theme--cell',
       renderCell: (params) => {
         return (
           <Button
@@ -232,8 +230,8 @@ const AllOverseasShipment = () => {
   }, []);
   return (
     <Box
-      component="main"
-      sx={{ flexGrow: 1, p: 0, width: "100%", overflowY: "hidden" }}
+      component='main'
+      sx={{ flexGrow: 1, p: 0, width: '100%', overflowY: 'hidden' }}
     >
       <DrawerHeader />
       {/* <Header
@@ -247,50 +245,50 @@ const AllOverseasShipment = () => {
         <Box>
           <Box
             style={{
-              display: "flex",
-              justifyContent: "space-between",
-              gap: "10px",
+              display: 'flex',
+              justifyContent: 'space-between',
+              gap: '10px',
             }}
           >
             {companyName ? (
-              <Box sx={{ display: "flex" }}>
-                <Table sx={{ background: "#024d70", borderRadius: "15px" }}>
+              <Box sx={{ display: 'flex' }}>
+                <Table sx={{ background: '#024d70', borderRadius: '15px' }}>
                   <tbody>
                     <TableRow>
                       <TableCell
                         sx={{
-                          fontWeight: "bold",
-                          fontSize: "15px",
-                          color: "#9198ed",
+                          fontWeight: 'bold',
+                          fontSize: '15px',
+                          color: '#9198ed',
                         }}
                       >
                         Company Name :
                       </TableCell>
-                      <TableCell style={{ color: "#fff" }}>
+                      <TableCell style={{ color: '#fff' }}>
                         {companyName}
                       </TableCell>
                       <TableCell
                         sx={{
-                          fontWeight: "bold",
-                          fontSize: "15px",
-                          color: "#9198ed",
+                          fontWeight: 'bold',
+                          fontSize: '15px',
+                          color: '#9198ed',
                         }}
                       >
                         Concern Person :
                       </TableCell>
-                      <TableCell style={{ color: "#fff" }}>
+                      <TableCell style={{ color: '#fff' }}>
                         {concernPerson}
                       </TableCell>
                       <TableCell
                         sx={{
-                          fontWeight: "bold",
-                          fontSize: "15px",
-                          color: "#9198ed",
+                          fontWeight: 'bold',
+                          fontSize: '15px',
+                          color: '#9198ed',
                         }}
                       >
                         Mobile :
                       </TableCell>
-                      <TableCell style={{ color: "#fff" }}>{mobile}</TableCell>
+                      <TableCell style={{ color: '#fff' }}>{mobile}</TableCell>
                     </TableRow>
                   </tbody>
                 </Table>
@@ -300,23 +298,23 @@ const AllOverseasShipment = () => {
             )}
 
             <ToggleButtonGroup
-              color="primary"
+              color='primary'
               value={toggleValue}
               exclusive
               onChange={(e) => {
                 setToggleValue(e.target.value);
               }}
-              aria-label="Platform"
+              aria-label='Platform'
             >
               <ToggleButton
                 classes={{ selected: classes.selected }}
-                value="pending"
+                value='pending'
               >
                 Pending
               </ToggleButton>
               <ToggleButton
                 classes={{ selected: classes.selected }}
-                value="recieved"
+                value='recieved'
               >
                 Recieved
               </ToggleButton>
@@ -325,26 +323,26 @@ const AllOverseasShipment = () => {
         </Box>
         <Box
           sx={{
-            width: "100%",
-            height: "68vh",
-            "& .super-app-theme--header": {
-              background: "#eee",
-              color: "black",
-              textAlign: "center",
+            width: '100%',
+            height: '68vh',
+            '& .super-app-theme--header': {
+              background: '#eee',
+              color: 'black',
+              textAlign: 'center',
             },
-            "& .vertical-lines .MuiDataGrid-cell": {
-              borderRight: "1px solid #e0e0e0",
+            '& .vertical-lines .MuiDataGrid-cell': {
+              borderRight: '1px solid #e0e0e0',
             },
-            "& .supercursor-app-theme--cell:hover": {
+            '& .supercursor-app-theme--cell:hover': {
               background:
-                "linear-gradient(180deg, #AA076B 26.71%, #61045F 99.36%)",
-              color: "white",
-              cursor: "pointer",
+                'linear-gradient(180deg, #AA076B 26.71%, #61045F 99.36%)',
+              color: 'white',
+              cursor: 'pointer',
             },
-            "& .MuiDataGrid-columnHeaderTitleContainer": {
-              background: "#eee",
+            '& .MuiDataGrid-columnHeaderTitleContainer': {
+              background: '#eee',
             },
-            position: "relative",
+            position: 'relative',
           }}
         >
           <DataGrid
@@ -352,32 +350,32 @@ const AllOverseasShipment = () => {
               NoRowsOverlay: () => (
                 <Box
                   sx={{
-                    width: "100%",
-                    height: "100%",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
                   }}
                 >
                   <Box
                     sx={{
                       // border: '2px solid blue',
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "center",
-                      width: "150px",
-                      height: "150px",
+                      display: 'flex',
+                      flexDirection: 'column',
+                      justifyContent: 'center',
+                      width: '150px',
+                      height: '150px',
                     }}
                   >
                     <img
                       src={Nodata}
-                      alt=""
-                      style={{ width: "100px", height: "100px" }}
+                      alt=''
+                      style={{ width: '100px', height: '100px' }}
                     />
 
                     <Typography
-                      variant="body2"
-                      sx={{ fontWeight: "bold", fontSize: "1rem" }}
+                      variant='body2'
+                      sx={{ fontWeight: 'bold', fontSize: '1rem' }}
                     >
                       No data found !
                     </Typography>
@@ -393,7 +391,7 @@ const AllOverseasShipment = () => {
             columns={columns}
             rows={rows}
             rowHeight={40}
-            Height={"85vh"}
+            Height={'85vh'}
           />
         </Box>
 
