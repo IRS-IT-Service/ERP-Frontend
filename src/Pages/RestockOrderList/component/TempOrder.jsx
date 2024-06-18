dbe3963f4c7d845c6c3a6395a87d43906204eb9d
IRS-Github <droneservicecenter@gmail.com>
2024-06-18 15:50:13 +0530

    added

--- src/Pages/RestockOrderList/component/TempOrder.jsx
+++ src/Pages/RestockOrderList/component/TempOrder.jsx
@@ -8,37 +8,45 @@ import {
   TableHead,
   TableRow,
   Paper,
-  TextField,
   Switch,
   FormControlLabel,
   Button,
+  TextField,
+  CircularProgress,
 } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Delete } from "@mui/icons-material";
-import { useAssignOrderToVendorMutation, useGetSingleVendorQuery } from "../../../features/api/RestockOrderApiSlice";
+import {
+  useAssignOrderVendorMutation,
+  useGetSingleVendorQuery,
+} from "../../../features/api/RestockOrderApiSlice";
 import { setOverseaseSelectedOrder } from "../../../features/slice/selectedItemsSlice";
 import { toast } from "react-toastify";
 
 const TempOrder = () => {
   const params = useParams();
   const id = params.id;
+  const navigate = useNavigate()
 
   const dispatch = useDispatch();
   const { selectedOverseaseOrder } = useSelector(
     (state) => state.SelectedItems
   );
-  const [useRMB, setUseRMB] = useState(true);
+  const [useRMB, setUseRMB] = useState(false);
   const [conversionRate, setConversionRate] = useState(null);
   const [orderData, setOrderData] = useState([]);
+  const [piNo,setPINumber] = useState(null)
 
   // rtk queries
   const { data: getVendor, isLoading } = useGetSingleVendorQuery(id);
-  const [assignOrder , {isLoading:assignOrderLoading}] = useAssignOrderToVendorMutation()
+  const [assignOrder, { isLoading: assignOrderLoading }] =
+  useAssignOrderVendorMutation();
 
   const handleToggleChange = () => {
     setUseRMB(!useRMB);
   };
+
   useEffect(() => {
     const initializedData = selectedOverseaseOrder.map((item) => ({
       ...item,
@@ -49,39 +57,39 @@ const TempOrder = () => {
     setOrderData(initializedData);
   }, [selectedOverseaseOrder]);
 
-  // getting the value after changint the c rates
+  // getting the value after changing the c rates
   const handleConversionRateChange = (e) => {
-    const rate = e.target.value;
+    const rate = parseFloat(e.target.value);
     setConversionRate(rate);
     const updatedData = orderData.map((item) => {
+      const updatedItem = { ...item };
       if (useRMB) {
-        return {
-          ...item,
-          usdValue: item.usdValue / rate,
-        };
+        updatedItem.usdValue = parseFloat(
+          ((item.rmbValue || 0) * rate * (item.orderQty || 1)).toFixed(2)
+        );
       } else {
-        return {
-          ...item,
-          rmbValue: item.rmbValue * rate,
-        };
+        updatedItem.rmbValue = parseFloat(
+          ((item.usdValue || 0) * rate * (item.orderQty || 1)).toFixed(2)
+        );
       }
+      return updatedItem;
     });
 
     setOrderData(updatedData);
   };
 
-  console.log(useRMB)
-
-  // changeing input value then we get the data
+  // changing input value then we get the data
   const handleInputChange = (index, field, value) => {
     const updatedData = [...orderData];
     updatedData[index][field] = value;
     if (field === "rmbValue" && useRMB) {
-      console.log("hielle")
-      updatedData[index].usdValue = value / conversionRate;
+      updatedData[index].usdValue = parseFloat(
+        (value * conversionRate * (updatedData[index].orderQty || 1)).toFixed(2)
+      );
     } else if (field === "usdValue" && !useRMB) {
-      console.log("ussdd")
-      updatedData[index].rmbValue = value * conversionRate;
+      updatedData[index].rmbValue = parseFloat(
+        (value * conversionRate * (updatedData[index].orderQty || 1)).toFixed(2)
+      );
     }
     setOrderData(updatedData);
   };
@@ -94,16 +102,19 @@ const TempOrder = () => {
     dispatch(setOverseaseSelectedOrder(updatedData));
   };
 
-  // get the totalvalue of usd
+  // get the total value of usd
   const totalUSDValues = useMemo(() => {
-    return orderData.reduce(
-      (total, item) => total + parseFloat(item.usdValue || 0),
-      0
-    );
+    return orderData
+      .reduce(
+        (total, item) =>
+          total + parseFloat(parseFloat(item.usdValue || 0).toFixed(2)),
+        0
+      )
+      .toFixed(2);
   }, [orderData]);
 
   // assign order
-  const handleAssignOrder = async() => {
+  const handleAssignOrder = async () => {
     for (let order of orderData) {
       const { SKU, orderQty, rmbValue, usdValue } = order;
       if (!SKU || !rmbValue || !usdValue || !orderQty)
@@ -115,19 +126,34 @@ const TempOrder = () => {
       Orderqty: +product.orderQty,
       RMB: +product.rmbValue,
       USD: +product.usdValue,
-      Gst: item.GST,
-      Brand: item.Brand,
-      prevRMB:item.prevRMB,
-      prevUSD:item.prevUSD
+      Gst: product.GST,
+      Brand: product.Brand,
+      prevRMB: product.prevRMB,
+      prevUSD: product.prevUSD,
     }));
-   try{
-    const result= await assignOrder(products).unwrap();
-    toast.success("Order added successfully")
-
-   }catch(error){
-    console.log(error)
-   }
+    const data = {
+      vendorId:id,
+      products:products,
+      piNo:piNo
+    }
+    try {
+      const result = await assignOrder(data).unwrap();
+      toast.success("Order added successfully");
+      navigate("/OverseasorderList")
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+  const getPlaceholderText = () => {
+    if (useRMB && conversionRate) {
+      return `1 RMB = ${(1 / conversionRate).toFixed(2)} USD`;
+    } else if (!useRMB && conversionRate) {
+      return `1 USD = ${(1 / conversionRate).toFixed(2)} RMB`;
+    }
+    return useRMB ? "1 RMB = ? USD" : "1 USD = ? RMB";
+  };
+
   return (
     <Box sx={{ marginTop: "70px" }}>
       <div
@@ -170,24 +196,33 @@ const TempOrder = () => {
       >
         <div>
           <FormControlLabel
-            control={<Switch checked={useRMB} onChange={handleToggleChange} />}
+            control={
+              <Switch
+                checked={!useRMB}
+                onChange={handleToggleChange}
+                color={useRMB === false ? "secondary" : "warning"}
+              />
+            }
             label={useRMB ? "RMB" : "USD"}
           />
           <input
-            placeholder={useRMB ? "1USD Per RMB": "1RMB Per USD"  }
-            value={conversionRate}
+            placeholder={getPlaceholderText()}
+            value={conversionRate || ""}
             onChange={handleConversionRateChange}
             type="number"
             style={{ ml: 2, width: "50%", padding: "4%" }}
           />
+        </div>
+        <div>
+          <TextField size="small" label={"Enter PI Number"} value={piNo} onChange={(e) => setPINumber(e.target.value)}></TextField>
         </div>
 
         <div>
           <span style={{ fontWeight: "bold", color: "blue" }}>TOTAL USD:</span>{" "}
           <span>{totalUSDValues}</span>
         </div>
-        <Button variant="contained" onClick={() => handleAssignOrder()}>
-          Order
+        <Button variant="contained" onClick={() => handleAssignOrder()} disabled={assignOrderLoading}>
+          {assignOrderLoading? <CircularProgress/> : "Order"}
         </Button>
       </div>
       <TableContainer component={Paper} sx={{ mt: 2, width: "88vw" }}>
@@ -234,68 +269,86 @@ const TempOrder = () => {
           </TableHead>
           <TableBody>
             {orderData.map((item, index) => (
-              <TableRow key={item.sku}>
+              <TableRow key={item.SKU}>
                 <TableCell sx={{ textAlign: "center", padding: "0px" }}>
                   {item.SKU}
                 </TableCell>
                 <TableCell sx={{ textAlign: "center", padding: "4px" }}>
                   {item.Name}
                 </TableCell>
-                <TableCell sx={{ textAlign: "center", padding: "0px" }}>
+                <TableCell sx={{ textAlign: "center", padding: "4px" }}>
                   {item.GST}
                 </TableCell>
-                <TableCell sx={{ textAlign: "center", padding: "0px" }}>
+                <TableCell sx={{ textAlign: "center", padding: "4px" }}>
                   {item.SoldCount}
                 </TableCell>
-                <TableCell sx={{ textAlign: "center", padding: "0px" }}>
+                <TableCell sx={{ textAlign: "center", padding: "4px" }}>
                   {item.Threshold}
                 </TableCell>
-                <TableCell sx={{ textAlign: "center", padding: "0px" }}>
+                <TableCell sx={{ textAlign: "center", padding: "4px" }}>
                   {item.prevRMB}
                 </TableCell>
-                <TableCell sx={{ textAlign: "center", padding: "0px" }}>
+                <TableCell sx={{ textAlign: "center", padding: "4px" }}>
                   {item.prevUSD}
                 </TableCell>
-                <TableCell sx={{ textAlign: "center", padding: "0px" }}>
+                <TableCell sx={{ textAlign: "center", padding: "4px" }}>
                   {item.Quantity}
                 </TableCell>
-                <TableCell sx={{ textAlign: "center", padding: "0px" }}>
-                  <input
-                    style={{ width: "40%", padding: "4%" }}
+                <TableCell sx={{ textAlign: "center", padding: "4px" }}>
+                  <TextField
+                    type="number"
+                    size="small"
+                    sx={{ width: "50px" }}
+                    required
                     value={item.orderQty}
                     onChange={(e) =>
-                      handleInputChange(index, "orderQty", e.target.value)
+                      handleInputChange(
+                        index,
+                        "orderQty",
+                        parseFloat(e.target.value)
+                      )
                     }
-                    type="number"
                   />
                 </TableCell>
-                <TableCell sx={{ textAlign: "center", padding: "0px" }}>
-                  <input
-                    style={{ width: "40%", padding: "4%" }}
+                <TableCell sx={{ textAlign: "center", padding: "4px" }}>
+                  <TextField
+                    type="number"
+                    size="small"
+                    sx={{ width: "110px" }}
                     value={item.rmbValue}
                     onChange={(e) =>
-                      handleInputChange(index, "rmbValue", e.target.value)
+                      handleInputChange(
+                        index,
+                        "rmbValue",
+                        parseFloat(e.target.value)
+                      )
                     }
-                    type="number"
+                    autoFocus={useRMB}
                     disabled={!useRMB}
                   />
                 </TableCell>
-                <TableCell sx={{ textAlign: "center" }}>
-                  <input
-                    style={{ width: "40%", padding: "4%" }}
-                    value={item.usdValue}
-                    onChange={(e) =>
-                      handleInputChange(index, "usdValue", e.target.value)
-                    }
+                <TableCell sx={{ textAlign: "center", padding: "4px" }}>
+                  <TextField
                     type="number"
+                    value={item.usdValue}
+                    size="small"
+                    sx={{ width: "110px" }}
+                    autoFocus={!useRMB}
+                    color="secondary"
+                    onChange={(e) =>
+                      handleInputChange(
+                        index,
+                        "usdValue",
+                        parseFloat(e.target.value)
+                      )
+                    }
                     disabled={useRMB}
                   />
                 </TableCell>
-                <TableCell>
-                  <Delete
-                    sx={{ color: "red" }}
-                    onClick={() => handleDeleteRow(index)}
-                  />
+                <TableCell sx={{ textAlign: "center", padding: "4px" }}>
+                  <Button onClick={() => handleDeleteRow(index)}>
+                    <Delete />
+                  </Button>
                 </TableCell>
               </TableRow>
             ))}
