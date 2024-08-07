6cd43476797639534d65b583cba306ef755263e4
IRS-Github <droneservicecenter@gmail.com>
2024-08-07 10:41:23 +0530

    added some comment

--- src/Pages/PackagingAndClient/createOrderShipment/RandDShipmentDial.jsx
+++ src/Pages/PackagingAndClient/createOrderShipment/RandDShipmentDial.jsx
@@ -26,12 +26,10 @@ import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import { useCreateShipmentMutation } from "../../../features/api/barcodeApiSlice";
 import { toast } from "react-toastify";
 
-const RandDShipmentDial = ({ open, setOpen, productData,refetchProduct }) => {
+const RandDShipmentDial = ({ open, setOpen, productData, refetchProduct }) => {
   const [name, setName] = useState("IRS R&D");
 
   const [rows, setRows] = useState(productData);
-
-
 
   // rtk query api callings
   const {
@@ -91,7 +89,7 @@ const RandDShipmentDial = ({ open, setOpen, productData,refetchProduct }) => {
       const create = await createShipment(formData).unwrap();
 
       toast.success("Shipment created successfully");
-      refetchProduct()
+      refetchProduct();
       setOpen(false);
     } catch (error) {
       console.log(error.message);
@@ -118,7 +116,7 @@ const RandDShipmentDial = ({ open, setOpen, productData,refetchProduct }) => {
             size="small"
             label="Individual"
             sx={{}}
-            InputLabelProps={{shrink:true}}
+            InputLabelProps={{ shrink: true }}
             value={getClientByName?.client?.ClientType}
           ></TextField>
           <TextField
@@ -126,15 +124,13 @@ const RandDShipmentDial = ({ open, setOpen, productData,refetchProduct }) => {
             label="Client Name"
             sx={{}}
             value={getClientByName?.client?.ContactName}
-            InputLabelProps={{shrink:true}}
-
+            InputLabelProps={{ shrink: true }}
           ></TextField>
           <TextField
             size="small"
             label="Contact No"
             sx={{}}
-            InputLabelProps={{shrink:true}}
-
+            InputLabelProps={{ shrink: true }}
             value={getClientByName?.client?.ContactNumber}
           ></TextField>
           <TextField
@@ -142,8 +138,7 @@ const RandDShipmentDial = ({ open, setOpen, productData,refetchProduct }) => {
             label="Alternate No"
             sx={{}}
             value={"N/A"}
-            InputLabelProps={{shrink:true}}
-
+            InputLabelProps={{ shrink: true }}
           ></TextField>
         </Box>
 
@@ -201,7 +196,7 @@ const RandDShipmentDial = ({ open, setOpen, productData,refetchProduct }) => {
             </Box>
           </Box>
         </Box>
-        {/* for Items Which is Get Requested  */}
+        {/* for Items Which is Get Requested  this update hapened for creating dial */}
 
         <Box>
           <span
@@ -221,65 +216,127 @@ const RandDShipmentDial = ({ open, setOpen, productData,refetchProduct }) => {
             <ArrowDownwardIcon sx={{ color: "blue" }} />
           </span>
           <Box>
-            <TableContainer component={Paper} sx={{ maxHeight: "400px",width:"60vw" }}>
+            <TableContainer
+              component={Paper}
+              sx={{ maxHeight: "400px", width: "60vw" }}
+            >
               <Table stickyHeader>
                 <TableHead>
                   <TableRow>
-                    <TableCell sx={{padding:1, height: "20px", backgroundColor: "#50124f" ,color:"#fff" }}>
+                    <TableCell
+                      sx={{
+                        padding: 1,
+                        height: "20px",
+                        backgroundColor: "#50124f",
+                        color: "#fff",
+                      }}
+                    >
                       S.No
                     </TableCell>
-                    <TableCell sx={{padding:1, height: "20px", backgroundColor: "#50124f" ,color:"#fff" }}>
+                    <TableCell
+                      sx={{
+                        padding: 1,
+                        height: "20px",
+                        backgroundColor: "#50124f",
+                        color: "#fff",
+                      }}
+                    >
                       SKU
                     </TableCell>
-                    <TableCell sx={{padding:1, height: "20px", backgroundColor: "#50124f" ,color:"#fff" }}>
+                    <TableCell
+                      sx={{
+                        padding: 1,
+                        height: "20px",
+                        backgroundColor: "#50124f",
+                        color: "#fff",
+                      }}
+                    >
                       Name
                     </TableCell>
-                    <TableCell sx={{padding:1, height: "20px", backgroundColor: "#50124f" ,color:"#fff" }}>
+                    <TableCell
+                      sx={{
+                        padding: 1,
+                        height: "20px",
+                        backgroundColor: "#50124f",
+                        color: "#fff",
+                      }}
+                    >
                       Brand
                     </TableCell>
-                    <TableCell sx={{padding:1, height: "20px", backgroundColor: "#50124f" ,color:"#fff" }}>
+                    <TableCell
+                      sx={{
+                        padding: 1,
+                        height: "20px",
+                        backgroundColor: "#50124f",
+                        color: "#fff",
+                      }}
+                    >
                       GST
                     </TableCell>
-                    <TableCell sx={{padding:1, height: "20px", backgroundColor: "#50124f" ,color:"#fff" }}>
+                    <TableCell
+                      sx={{
+                        padding: 1,
+                        height: "20px",
+                        backgroundColor: "#50124f",
+                        color: "#fff",
+                      }}
+                    >
                       Actual Qty
                     </TableCell>
-                    <TableCell sx={{padding:1, height: "20px", backgroundColor: "#50124f" ,color:"#fff" }}>
+                    <TableCell
+                      sx={{
+                        padding: 1,
+                        height: "20px",
+                        backgroundColor: "#50124f",
+                        color: "#fff",
+                      }}
+                    >
                       Requested Qty
                     </TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody sx={{ overflowY: "auto" }}>
-                  {rows.length > 0 ? rows.map((row, index) => (
-                    <TableRow key={index}>
-                      <TableCell>{index + 1}</TableCell>
-                      <TableCell sx={{ width: "20%", textAlign: "center" }}>
-                        {row.SKU}
-                      </TableCell>
-                      <TableCell sx={{ width: "25%", textAlign: "center" }}>
-                        {row.Name}
-                      </TableCell>
-                      <TableCell sx={{ textAlign: "center" }}>
-                        {row.Brand}
-                      </TableCell>
-                      <TableCell sx={{ textAlign: "center" }}>
-                        {row.GST}
-                      </TableCell>
-                      <TableCell sx={{ textAlign: "center" }}>
-                        {row.ActualQuantity}
-                      </TableCell>
-                      <TableCell sx={{ textAlign: "center" }}>
-                        <TextField
-                          sx={{ width: "90px" }}
-                          type="number"
-                          value={row.Count}
-                          name="Count"
-                          onChange={(e) => handleQtyChange(row, e)}
-                          variant="outlined"
-                          size="small"
-                        />
+                  {rows.length > 0 ? (
+                    rows.map((row, index) => (
+                      <TableRow key={index}>
+                        <TableCell>{index + 1}</TableCell>
+                        <TableCell sx={{ width: "20%", textAlign: "center" }}>
+                          {row.SKU}
+                        </TableCell>
+                        <TableCell sx={{ width: "25%", textAlign: "center" }}>
+                          {row.Name}
+                        </TableCell>
+                        <TableCell sx={{ textAlign: "center" }}>
+                          {row.Brand}
+                        </TableCell>
+                        <TableCell sx={{ textAlign: "center" }}>
+                          {row.GST}
+                        </TableCell>
+                        <TableCell sx={{ textAlign: "center" }}>
+                          {row.ActualQuantity}
+                        </TableCell>
+                        <TableCell sx={{ textAlign: "center" }}>
+                          <TextField
+                            sx={{ width: "90px" }}
+                            type="number"
+                            value={row.Count}
+                            name="Count"
+                            onChange={(e) => handleQtyChange(row, e)}
+                            variant="outlined"
+                            size="small"
+                          />
+                        </TableCell>
+                      </TableRow>
+                    ))
+                  ) : (
+                    <TableRow>
+                      <TableCell sx={{ width: "100%" }}>
+                        <Typography sx={{ textAlign: "center" }}>
+                          No Itemes Requested
+                        </Typography>
                       </TableCell>
                     </TableRow>
-                  )): <TableRow ><TableCell sx={{width:"100%"}} ><Typography sx={{textAlign:"center"}}>No Itemes Requested</Typography></TableCell></TableRow>}
+                  )}
                 </TableBody>
               </Table>
             </TableContainer>
@@ -293,7 +350,11 @@ const RandDShipmentDial = ({ open, setOpen, productData,refetchProduct }) => {
           alignItems: "center",
         }}
       >
-        <Button variant="contained" onClick={() => handleSubmitShipment()} disabled = {rows.length === 0}>
+        <Button
+          variant="contained"
+          onClick={() => handleSubmitShipment()}
+          disabled={rows.length === 0}
+        >
           {createShipmentLoading ? <CircularProgress /> : "Create Shipment"}
         </Button>
         <Button sx={{ color: "red" }} onClick={handleClose}>
